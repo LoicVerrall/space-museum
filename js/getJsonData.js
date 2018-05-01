f83ca1ab80d79c0f0656f4b22fd58b84e3980c5d
Loic Verrall <loic.verrall@gmail.com>
2018-05-01 02:22:57 +0100

    Minor changes

--- js/getJsonData.js
+++ js/getJsonData.js
@@ -1,13 +1,12 @@
 // JavaScript Document
-$(document).ready(function(){
+$(document).ready(function () {
+  // AJAX service request to get the main text data from the json data store
+  $.getJSON('../model/data.json', function (jsonObj) {
+    // Get Space Shuttle data from backend.
+    const spaceshuttleJson = jsonObj.modelTextData.spaceshuttle
 
-	 //AJAX service request to get the main text data from the json data store
-	 $.getJSON('../model/data.json', function(jsonObj) {
-
-		// Get Space Shuttle data from backend.
-		const spaceshuttleJson = jsonObj.modelTextData.spaceshuttle;
-		$('#spaceshuttle_title').html('<h4>' + spaceshuttleJson.title + '<h4>');
-		$('#spaceshuttle_description').html('<p>' + spaceshuttleJson.description + '<p>');
-		$('#spaceshuttle_link').html('<a href="' + spaceshuttleJson.link + '" class="btn btn-info">More Info</a>');
-	 });
-});
+    $('#spaceshuttle_title').html('<h4>' + spaceshuttleJson.title + '<h4>')
+    $('#spaceshuttle_description').html('<p>' + spaceshuttleJson.description + '<p>')
+    $('#spaceshuttle_link').html('<a href="' + spaceshuttleJson.link + '" class="btn btn-info">More Info</a>')
+  })
+})
