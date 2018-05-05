bb50e4411d1f9aa6e18c5058fcbec7004cdfec46
Loic Verrall <loic.verrall@gmail.com>
2018-05-05 04:06:40 +0100

    Camera view buttons and headlight toggle now works

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -8,3 +8,26 @@ function toggleModelSpinning () {
   modelIsSpinning = !modelIsSpinning
   document.getElementById('model__RotationTimer-67-TIMER').setAttribute('enabled', modelIsSpinning.toString())
 }
+
+function toggleWireframeMode () {
+  document.getElementById('model').runtime.togglePoints(true)
+}
+
+var headlightOn = true
+
+function toggleHeadlight () {
+  headlightOn = !headlightOn
+  document.getElementById('model__Headlight-63').setAttribute('headlight', headlightOn.toString())
+}
+
+function cameraFront () {
+  document.getElementById('model__CameraFront-61').setAttribute('bind', 'true')
+}
+
+function cameraSide () {
+  document.getElementById('model__CameraSide-68').setAttribute('bind', 'true')
+}
+
+function cameraBottom () {
+  document.getElementById('model__CameraBottom-70').setAttribute('bind', 'true')
+}
