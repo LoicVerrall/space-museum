63f876a60bbd9490c34d53667698f39cf473b5f3
Loic Verrall <loic.verrall@gmail.com>
2018-05-05 03:24:35 +0100

    Model doesn't spin on page load

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -2,9 +2,9 @@ $(document).ready(function () {
   $('[data-toggle="popover"]').popover()
 })
 
-var modelIsSpinning = true
+var modelIsSpinning = false
 
 function toggleModelSpinning () {
   modelIsSpinning = !modelIsSpinning
-  document.getElementById('model__TimeSensor001-67-TIMER').setAttribute('enabled', modelIsSpinning.toString())
+  document.getElementById('model__RotationTimer-67-TIMER').setAttribute('enabled', modelIsSpinning.toString())
 }
