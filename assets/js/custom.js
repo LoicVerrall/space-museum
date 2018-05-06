9f9e85b885820afd76ad640e83193814ffc859bb
Loic Verrall <loic.verrall@gmail.com>
2018-05-06 16:15:12 +0100

    Saturn model added.
    Assumed names of elements in `x3d` files changed slightly

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -6,28 +6,28 @@ var modelIsSpinning = false
 
 function toggleModelSpinning () {
   modelIsSpinning = !modelIsSpinning
-  document.getElementById('model__RotationTimer-67-TIMER').setAttribute('enabled', modelIsSpinning.toString())
+  document.getElementById('model__RotationTimer').setAttribute('enabled', modelIsSpinning.toString())
 }
 
 function toggleWireframeMode () {
   document.getElementById('model').runtime.togglePoints(true)
 }
 
-var headlightOn = true
+var headlightOn = false
 
 function toggleHeadlight () {
   headlightOn = !headlightOn
-  document.getElementById('model__Headlight-63').setAttribute('headlight', headlightOn.toString())
+  document.getElementById('model__Headlight').setAttribute('headlight', headlightOn.toString())
 }
 
 function cameraFront () {
-  document.getElementById('model__CameraFront-61').setAttribute('bind', 'true')
+  document.getElementById('model__CameraFront').setAttribute('bind', 'true')
 }
 
 function cameraSide () {
-  document.getElementById('model__CameraSide-68').setAttribute('bind', 'true')
+  document.getElementById('model__CameraSide').setAttribute('bind', 'true')
 }
 
 function cameraBottom () {
-  document.getElementById('model__CameraBottom-70').setAttribute('bind', 'true')
+  document.getElementById('model__CameraBottom').setAttribute('bind', 'true')
 }
--- todo.md
+++ todo.md
@@ -2,6 +2,10 @@
 
 ### 3D Content
 - Smooth Falcon Heavy model.
+- When importing a new model:
+  1. Replace the `stopTime` attribute of the **RotationTimer** element with `enabled='false'`.
+  2. Increase the `cycleInterval` parameter to **3**.
+  3. Find and replace the names of all the cameras, rotation timers, headlights, etc, to remove the numbers.
 
 ### Embedding 3D Content
 - Load content (and images, possibly sounds/videos) from database.
